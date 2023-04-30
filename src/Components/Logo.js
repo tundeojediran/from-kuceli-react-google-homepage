5a59c4f9b38cebd2010411d37549f9629633aa8c
Daniel Tunde Ojediran <tundeojediran@gmail.com>
2023-04-30 17:38:05 +0100

    add alt attribute to img tag

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -4,7 +4,7 @@ export default function Logo() {
       {" "}
       <img
         src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-        className="logo"
+        className="logo" alt="logo"
       />
     </div>
   );
